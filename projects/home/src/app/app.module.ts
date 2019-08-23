cf1bfd9c189f24748c7d11cfb5cc47f5ebecffd3
Shakil Khan <shakil.khan@kaplan.com>
2019-08-23 01:54:20 -0400

    add top-nav component

--- projects/home/src/app/app.module.ts
+++ projects/home/src/app/app.module.ts
@@ -7,18 +7,18 @@ import { AppComponent } from './app.component';
 // library
 import { Ng7CommonModule } from '@ng7/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Ng8MaterialModule } from '@ng8/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     Ng7CommonModule,
+    Ng8MaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- projects/ng7-common/src/lib/ng7-common.module.ts
+++ projects/ng7-common/src/lib/ng7-common.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { Ng8MaterialModule } from '@ng8/material';
 
 @NgModule({
-  declarations: [FooterComponent],
-  imports: [
-  ],
-  exports: [FooterComponent]
+  declarations: [FooterComponent, TopNavComponent],
+  imports: [Ng8MaterialModule],
+  exports: [FooterComponent, TopNavComponent] // <-------------
 })
-export class Ng7CommonModule { }
+export class Ng7CommonModule {}
